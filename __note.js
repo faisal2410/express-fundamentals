5292500f42e021d2a94426eb6cb480c197dc6289
Faisal Ahmed <faisal2410@yahoo.com>
2022-09-16 20:30:23 +0600

    initial commit

---
+++ __note.js
@@ -0,0 +1,51 @@
+/**
+ * Express website
+ * https://expressjs.com/
+ * HTTp request methods
+ * https://developer.mozilla.org/en-US/docs/Web/HTTP/Methods
+ * Express-Gateway
+ * https://www.express-gateway.io/
+ * 
+ * Frameworks built on Express
+ * https://expressjs.com/en/resources/frameworks.html
+ * 
+ * 
+ * */ 
+
+/**
+ * Fast, unopinionated, minimalist web framework for Node.js
+ *  NODEJS IS the language
+// Express is node, a node module
+ *  express is a 3rd party module
+ * Express is a routing and middleware web framework that has minimal functionality of its own: An Express application is essentially a series of middleware function calls.
+ * 
+ * 
+ *  An "app" is the express function (createAppliction inside the Express module)
+// invoked and is an Express appliction
+ * 
+ * 
+ * */ 
+
+/**
+ * // app object has a few methods:
+// HTTP verbs! REST verbs!
+// CRUD app cooresponence!
+// 1. get - READ
+// - DEFAULT for all browsers is get. 
+// 2. post - CREATE
+// 3. delete - DELETE
+// 4. put - UPDATE
+// 5. all - i will accept any method
+
+// Take 2 args:
+// 1. path
+// 2. callback to run if an HTTP request that matchs THIS verb
+// is made to the path in #1
+// app.all('/',(req,res)=>{
+//     res.send(`<h1>Welcome to the home page!`)
+// })
+ * 
+ * 
+ * 
+ * 
+ * */ 
