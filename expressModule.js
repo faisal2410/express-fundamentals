7956c526534f38a4725187accd83ad4591538829
Faisal Ahmed <faisal2410@yahoo.com>
2022-09-18 10:32:19 +0600

    updated for class

--- expressModule.js
+++ expressModule.js
@@ -1,3 +1,43 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const helmet=require('helmet');
+app.use(helmet())
+
+app.use(express.static('public'));
+app.use(express.json())
+app.use(express.urlencoded({extended:false}));
+
+// app.all('*',(req, res)=>{    
+//     res.send('<h1>Hello World</h1>')
+// });
+
+// app.all('*',(req, res)=>{    
+//     res.json('<h1>Hello World</h1>')
+// });
+app.all('*',(req, res)=>{    
+    res.json({subject:"Express js Fundamentals", message:"Welcome to Ostad",})
+});
+
+// app.all('/',(req, res)=>{    
+//     console.log(path.join(__dirname + '/node.html'))
+//     res.sendFile(path.join(__dirname + '/node.html'))   
+// });
+
+// app.get('/',(req, res)=>{
+//     console.log(req)
+//     res.send(`<h1>Welcome to the home GET page!`)
+// })
+// app.post('/',(req, res)=>{
+//     res.send(`<h1>Welcome to the home POST page!`)
+// })
+// app.delete('/',(req, res)=>{
+
+// })
+// app.put('/',(req, res)=>{
+
+// })
+
+
 app.listen(3000)
+console.log("Server Run Successfully")
