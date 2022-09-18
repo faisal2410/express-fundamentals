0f4adbf004a6967f904d3171e64785a7890189c0
Faisal ahmed <faisal2410@yahoo.com>
2022-09-19 02:33:09 +0600

    res.json() added

--- app.js
+++ app.js
@@ -22,6 +22,17 @@ app.get("/three", function (req, res) {
 
 
 // JSON Response
+
+/**
+res.json([body])
+Sends a JSON response. This method sends a response (with the correct content-type) that is the parameter converted to a JSON string using JSON.stringify().
+
+The parameter can be any JSON type, including object, array, string, Boolean, number, or null, and you can also use it to convert other values to JSON.
+res.json(null)
+res.json({ user: 'tobi' })
+res.status(500).json({ error: 'message' })
+
+*/ 
 app.get("/four", function (req, res) {
     let myJSONArray = [
         {
